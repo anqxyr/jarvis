4a76db43fd8e1e8cd6f218e57b937d2d921e2537
anqxyr <anqxyr@gmail.com>
2016-03-26 19:47:57 +0300

    improve matching and fix minor bugs

--- pyscp_bot/modules/scp.py
+++ pyscp_bot/modules/scp.py
@@ -8,7 +8,6 @@
 
 import pyscp
 import sopel
-import difflib
 import random
 
 ###############################################################################
@@ -44,8 +43,7 @@ def setup(bot):
     #pyscp.utils.default_logging(True)
     bot._wiki = pyscp.wikidot.Wiki('scp-wiki')
     bot._wiki.auth('pyscp_bot', bot.config.scp.wikipass)
-    bot._pages = list(bot._wiki.list_pages(
-        body='title created_by rating tags'))
+    refresh_page_cache(bot)
     bot._found = []
 
 
@@ -65,34 +63,40 @@ def author(bot, trigger):
     """Display basic author statistics."""
     partname = trigger.group(2)
     if not partname:
-        partname = trigger.user
-    authors = {p.author for p in bot._pages if p.author}
-    candidates = difflib.get_close_matches(partname, authors)
-    if not candidates:
+        partname = trigger.nick
+    authors = list({
+        p.author for p in bot._pages
+        if p.author and partname.lower() in p.author.lower()})
+    if not authors:
         _say(bot, trigger, NOAUTHOR)
         return
-    author = candidates[0]
+    if len(authors) > 1:
+        bot.say('{}: did you mean {} or {}?'.format(
+            trigger.nick.lstrip('~'),
+            ', '.join(authors[:-1]),
+            authors[-1]))
+    author = authors[0]
     pages = [
-        p for p in bot._pages if p.author == author and
-        ('scp' in p.tags or 'tale' in p.tags)]
+        p for p in bot._pages if p.author == author if '_sys' not in p.tags]
+    skips = {p for p in pages if 'scp' in p.tags}
+    tales = {p for p in pages if 'tale' in p.tags}
+    goifs = {p for p in pages if 'goi-format' in p.tags}
+    pages = (skips | tales | goifs)
+    rating = sum(p.rating for p in pages)
     msg = (
         '{} has written {} SCPs, {} tales, and {} GOI-format pages. '
-        'They have {} net upvotes with an average of {}.')
-    msg = msg.format(
-        author,
-        len([p for p in pages if 'scp' in p.tags]),
-        len([p for p in pages if 'tale' in p.tags]),
-        len([p for p in pages if 'goi-format' in p.tags]),
-        sum([p.rating for p in pages]),
-        sum([p.rating for p in pages]) // len(pages))
+        'They have {} net upvotes with an average of {}.'
+    ).format(
+        author, len(skips), len(tales), len(goifs),
+        rating, rating // len(pages))
     bot.say(msg)
 
 
-@sopel.module.rule('^(scp-[0-9]+)$')
-@sopel.module.rule('.*!(scp-[0-9]+)')
+@sopel.module.rule(r'(?i)^(scp-[0-9]+)$')
+@sopel.module.rule(r'(?i).*!(scp-[0-9]+)')
 def skip(bot, trigger):
     """Display SCP article details."""
-    name = trigger.group(1)
+    name = trigger.group(1).lower()
     page_matches = [p for p in bot._pages if p._body.name == name]
     if not page_matches:
         _say(bot, trigger, NOPAGE)
@@ -102,22 +106,18 @@ def skip(bot, trigger):
 
 @sopel.module.commands('s', 'sea', 'search')
 def search(bot, trigger):
-    partname = trigger.group(2)
-    titles = [p.title for p in bot._pages]
-    candidates = difflib.get_close_matches(partname, titles, 10, 0.2)
-    if not candidates:
+    partname = trigger.group(2).lower()
+    pages = [p for p in bot._pages if partname in p.title.lower()]
+    if not pages:
         _say(bot, trigger, NOPAGE)
         return
-    elif len(candidates) == 1:
-        bot.say(_page_info(
-            next(p for p in bot._pages if p.title == candidates[0])))
+    if len(pages) == 1:
+        bot.say(_page_info(pages[0]))
         return
-    elif len(candidates) <= 3:
-        msg = '; '.join(candidates)
-    else:
-        msg = '{} plus {} more.'.format(
-            '; '.join(candidates[:3]), len(candidates) - 3)
-    bot._found = candidates
+    msg = ' || '.join([p.title for p in pages][:3])
+    if len(pages) > 3:
+        msg += ' plus {} more.'.format(len(pages) - 3)
+    bot._found = pages
     bot.say(msg)
 
 
@@ -126,17 +126,15 @@ def showmore(bot, trigger):
     index = int(trigger.group(2)) - 1
     if index >= len(bot._found):
         _say(bot, trigger, OUTOFRANGE)
-    bot.say(_page_info(
-        next(p for p in bot._pages if p.title == bot._found[index])))
+    bot.say(_page_info(bot._found[index]))
 
 
 @sopel.module.commands('lc', 'lastcreated')
 def lastcreated(bot, trigger):
     """Display recently created pages."""
-    pages = bot._wiki.list_pages(
-        order='created_at desc', limit=3, body='rating')
-    for p in pages:
-        bot.say(_page_info(p))
+    pages = list(bot._wiki.list_pages(
+        order='created_at desc', limit=3, body='rating'))
+    bot.say(' || '.join([_page_info(p) for p in pages]))
 
 
 @sopel.module.interval(3600)
@@ -149,8 +147,10 @@ def refresh_page_cache(bot):
 
 def _page_info(page):
     msg = '{} (written by {}; rating: {:+d}) - {}'
-    return msg.format(page.title, page.author, page.rating, page.url)
+    return msg.format(
+        page.title, page.author, page.rating,
+        page.url.replace('scp-wiki.wikidot.com', 'scp-wiki.net'))
 
 
 def _say(bot, trigger, messages):
-    bot.say('{}: {}'.format(trigger.user.lstrip('~'), random.choice(messages)))
+    bot.say('{}: {}'.format(trigger.nick.lstrip('~'), random.choice(messages)))
