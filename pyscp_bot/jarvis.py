ad3b69dd74fe535e0a40de872b6036694780c97e
anqxyr <anqxyr@gmail.com>
2016-03-27 23:13:32 +0300

    fix missing comma

--- pyscp_bot/jarvis.py
+++ pyscp_bot/jarvis.py
@@ -74,7 +74,7 @@ def banlist_update_failed(user):
 
 def profane_username(user):
     messages = [
-        'kicking {user}: they have a filty mouth that could use some soap.'
+        'kicking {user}: they have a filty mouth that could use some soap.',
         "{user} is just untasteful, and I don't want their ilk here.",
         'Was {user} a troll? It was probably a troll.']
     return random.choice(messages).format(user=user.lstrip('~'))
@@ -86,3 +86,9 @@ def user_in_banlist(user, banned):
         '{user} (better known as {banned}) was exiled from this fine and pure kingdom.',
         '{user} was kicked and banned because they look like {banned} and their face smells.']
     return random.choice(messages).format(user=user, banned=banned)
+
+
+def user_never_seen(user):
+    messages = [
+        '{user}: I never saw anyone by that name here.']
+    return random.choice(messages).format(user=user.lstrip('~'))
