bfd2aeccdc6bcec1733ba96483ddee450884d14c
anqxyr <anqxyr@gmail.com>
2016-03-30 21:10:19 +0300

    remove db buffering

--- pyscp_bot/db.py
+++ pyscp_bot/db.py
@@ -5,22 +5,17 @@
 ###############################################################################
 
 import concurrent.futures
-import logging
 import peewee
-import queue
 
 ###############################################################################
 # Global Constants And Variables
 ###############################################################################
 
-log = logging.getLogger(__name__)
 pool = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-queue = queue.Queue()
 
 
 def queue_execution(fn, args=(), kwargs={}):
-    queue.put(dict(fn=fn, args=args, kwargs=kwargs))
-    pool.submit(async_write)
+    pool.submit(fn, *args, **kwargs)
 
 ###############################################################################
 # Database ORM Classes
@@ -62,34 +57,3 @@ class Message(BaseModel):
     channel = peewee.CharField()
     time = peewee.CharField()
     text = peewee.TextField()
-
-
-###############################################################################
-# Helper Functions
-###############################################################################
-
-
-def async_write(buffer=[]):
-    item = queue.get()
-    buffer.append(item)
-    if len(buffer) > 500 or queue.empty():
-        log.debug('Processing {} queue items.'.format(len(buffer)))
-        with db.transaction():
-            write_buffer(buffer)
-        buffer.clear()
-
-
-def write_buffer(buffer):
-    for item in buffer:
-        try:
-            item['fn'](*item.get('args', ()), **item.get('kwargs', {}))
-        except:
-            log.exception(
-                'Exception while processing queue item: {}'
-                .format(item))
-        queue.task_done()
-
-
-def create_tables(*tables):
-    for table in tables:
-        eval(table).create_table()
