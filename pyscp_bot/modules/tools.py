22aa6e750a191adf14832de76a5c245f0414e6a4
anqxyr <anqxyr@gmail.com>
2016-03-31 14:57:13 +0300

    fix command autocomplete

--- pyscp_bot/modules/tools.py
+++ pyscp_bot/modules/tools.py
@@ -13,29 +13,22 @@ import pyscp_bot.jarvis as vocab
 
 @sopel.module.commands('[^ ]+')
 def autocomplete(bot, trigger):
-    commands = {command: module
-                for module, group in bot._command_groups.items()
-                for command in group}
+    funcs = [f for group in bot._callables.values() for f in group.values()]
+    funcs = [f for l in funcs for f in l if hasattr(f, 'commands')]
     partial = trigger.group(1)
-    if partial in commands:
+    if any(partial in f.commands for f in funcs):
         return
-    matches = {k: v for k, v in commands.items() if k.startswith(partial)}
-    if not matches:
+    funcs = [
+        f for f in funcs if any(c.startswith(partial) for c in f.commands)]
+    if not funcs:
         return
-    if len(matches) == 1:
-        name, module = list(matches.items())[0]
-        items = [i for pr, gr in bot._callables.items() for i in gr.items()]
-        funcs = [f[0] for regexp, f in items]
-        funcs = [
-            f for f in funcs if f.__module__ == module and f.__name__ == name]
-        if not funcs:
-            return
+    if len(funcs) > 1:
+        names = [f.commands[0] for f in funcs]
+        bot.say('{}: did you mean {} or {}?'.format(
+            trigger.nick, ', '.join(names[:-1]), names[-1]))
+    else:
         wrapper = bot.SopelWrapper(bot, trigger)
         bot.call(funcs[0], wrapper, trigger)
-        return
-    commands = ['\x02{}\x02'.format(k) for k in matches]
-    bot.say('{}: did you mean {} or {}?'.format(
-        trigger.nick, ', '.join(commands[:-1]), commands[-1]))
 
 
 @sopel.module.commands('choose')
