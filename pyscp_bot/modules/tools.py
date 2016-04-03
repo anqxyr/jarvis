33a441c7435461ae914adcb6857d15309a1c2f79
anqxyr <anqxyr@gmail.com>
2016-04-03 14:25:11 +0300

    update tools

--- pyscp_bot/modules/tools.py
+++ pyscp_bot/modules/tools.py
@@ -6,8 +6,7 @@
 
 import random
 import sopel
-import pyscp_bot.jarvis as lexicon
-import re
+import pyscp_bot as jarvis
 
 ###############################################################################
 
@@ -46,36 +45,5 @@ def choose(bot, tr):
 @sopel.module.rule(r'(?i)(^(?:[+-]?[0-9]*d(?:[0-9]+|f))+(?:[+-][0-9]+)?$)')
 @sopel.module.commands('roll', 'dice')
 def dice(bot, tr):
-    groups = tr.groups()
-    expr = groups[0] if len(groups) == 1 else groups[1]
-    rolls = re.findall(r'([+-]?)([0-9]*)d([0-9]+|f)', expr.lower())
-    total = 0
-    output = []
-    for sign, count, sides in rolls:
-        count = 1 if not count else int(count)
-        if count > 5000:
-            bot.send(lexicon.die_count_too_high())
-            return
-        results = roll_die(sign, count, sides)
-        total += sum(results)
-        if sides != 'f':
-            output.extend(results)
-            continue
-        for value in results:
-            if value == 1:
-                output.append('\x033+\x0F')
-            elif value == -1:
-                output.append('\x034-\x0F')
-            else:
-                output.append(0)
-    bot.send('{} ({}={})'.format(total, expr, ', '.join(map(str, output))))
-
-
-def roll_die(sign, count, sides):
-    if sides != 'f':
-        results = [random.randint(1, int(sides)) for _ in range(count)]
-    else:
-        results = [random.randint(-1, 1) for _ in range(count)]
-    if sign == '-':
-        results = [i * -1 for i in results]
-    return results
+    inp = tr.groups()[0] if len(tr.groups()) == 1 else tr.groups()[1]
+    bot.send(jarvis.tools.roll_dice(inp))
