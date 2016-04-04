4b460875ea9ea5a1f2fdeb3681b22b6e9585ad13
anqxyr <anqxyr@gmail.com>
2016-04-04 12:54:45 +0300

    better !author command

--- pyscp_bot/scp.py
+++ pyscp_bot/scp.py
@@ -15,25 +15,25 @@ from . import ext
 
 def get_author_summary(pages, name):
     au = ext.User(pages, name)
+
     au_pages = pages(tags='author', author=name)
-    au_url = au_pages[0].url if au_pages else None
+    url = '({0[0].url}) '.format(au_pages) if au_pages else ''
+    url = url.replace('scp-wiki.wikidot.com', 'www.scp-wiki.net')
+    wrote = [
+        '\x02{0[scp].count}\x02 SCPs',
+        '\x02{0[tale].count}\x02 tales',
+        '\x02{0[goi-format].count}\x02 GOI-format pages',
+        '\x02{0[artwork].count}\x02 artwork galleries']
+    wrote = [i.format(au) for i in wrote if '\x020\x02' not in i.format(au)]
+    wrote = '({}) '.format(', '.join(wrote))
+    rewrite = ' ({} rewrites)'.format(au.rewrites.count) if au.rewrites else ''
 
-    template = """
-    *{au.name}* ({au_url}) has written *{au.pages.count}* pages
-    (*{au[scp].count}* SCPs,
-     *{au[tale].count}* tales,
-     *{au[goi-format].count}* GOI-format pages,
-     *{au[artwork].count}* artwork galleries)
-    (*{au.rewrites.count}* rewrites).
-    They have *{au.pages.rating}* net upvotes
-    with an average of *{au.pages.average_rating}*.
-    Their latest page is *{last.title}* at *{last.rating:+d}*."""
+    pages = '\x02{}\x02 {}has written \x02{}\x02 pages {}{}.'.format(
+        au.name, url, au.pages.count, wrote, rewrite)
+    rating = (
+        'They have \x02{}\x02 net upvotes with an average of \x02{}\x02.'
+        .format(au.pages.rating, au.pages.average))
+    last = 'Their latest page is \x02{}\x02 at \x02{:+d}\x02.'.format(
+        au.pages[0].title, au.pages[0].rating)
 
-    template = ' '.join(template.split())
-    output = template.format(au=au, au_url=au_url, last=au.pages[0])
-
-    output = re.sub(r'\*?(0|None)\*?[ \w-]*(?=(\)|,))', '', output)
-    output = re.sub(r' \(\)|(, (?=[,\)]))|((?<=\(), )', '', output)
-    output = output.replace('scp-wiki.wikidot.com', 'www.scp-wiki.net')
-    output = output.replace('*', '\x02')
-    return output
+    return ' '.join([pages, rating, last])
