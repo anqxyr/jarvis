1cfa4340fca5a71489ed801317ad53a498f94978
anqxyr <anqxyr@gmail.com>
2016-04-05 17:27:33 +0300

    add author stats generation and more scp stuff

--- jarvis/scp.py
+++ jarvis/scp.py
@@ -5,6 +5,8 @@
 # Module Imports
 ###############################################################################
 
+import re
+
 from . import ext
 from . import tools
 from . import lexicon
@@ -15,7 +17,6 @@ from . import lexicon
 
 
 def find_author(pages, partial, key):
-    """this is a test message"""
     authors = {p.author for p in pages}
     matches = sorted(a for a in authors if partial.lower() in a.lower())
     if not matches:
@@ -23,9 +24,8 @@ def find_author(pages, partial, key):
     elif len(matches) == 1:
         return get_author_summary(pages, matches[0])
     else:
-        tools.save_search(
-            lambda x: get_author_summary(pages, x), matches, key)
-        return lexicon.multiple_matches(matches)
+        tools.remember(matches, key, lambda x: get_author_summary(pages, x))
+        return lexicon.unclear_input(matches)
 
 
 def find_page(pages, partial, key):
@@ -36,8 +36,8 @@ def find_page(pages, partial, key):
     elif len(matches) == 1:
         return get_page_summary(matches[0])
     else:
-        tools.save_search(matches, get_page_summary, key)
-        return lexicon.multiple_matches([i.title for i in matches])
+        tools.remember(matches, key, get_page_summary)
+        return display_search_results([i.title for i in matches])
 
 
 def find_scp(pages, partial, key):
@@ -46,6 +46,36 @@ def find_scp(pages, partial, key):
 
 def find_tale(pages, partial, key):
     return find_page([p for p in pages if 'scp' in p.tags], partial, key)
+
+
+def find_tags(pages, tags, key):
+    matches = [p for p in pages if p.tags >= set(tags)]
+    if not matches:
+        return lexicon.page_not_found()
+    elif len(matches) == 1:
+        return get_page_summary(matches[0])
+    else:
+        tools.remember(matches, key, get_page_summary)
+        return display_search_results([i.title for i in matches])
+
+
+def lookup_url(pages, url):
+    if '/forum/' in url:
+        return
+    url = url.replace('www.scp-wiki.net', 'scp-wiki.wikidot.com')
+    pages = [p for p in pages if p.url == url]
+    if not pages:
+        return lexicon.page_not_found()
+    else:
+        return get_page_summary(pages[0])
+
+
+def display_search_results(results):
+    head, tail = results[:3], results[3:]
+    output = ', '.join('\x02{}\x02'.format(i) for i in head)
+    if tail:
+        output += ' and {} more...'.format(len(tail))
+    return output
 
 ###############################################################################
 # Output Generation Functions
@@ -96,3 +126,66 @@ def get_page_summary(page):
     output = '\x02{0.title}\x02 ({1}; rating: {0.rating:+d}) - {0.url}'.format(
         page, authors)
     return output.replace('scp-wiki.wikidot.com', 'www.scp-wiki.net')
+
+
+def get_author_details(pages, name):
+    au = ext.User(pages, name)
+
+    counts = [
+        '||Pages:||{0.pages.count}||',
+        '||● SCPs:||{0[scp].count}||',
+        '||● Tales:||{0[tale].count}||',
+        '||● GOI-format:||{0[goi-format].count}||',
+        '||● Artwork:||{0[artwork].count}||',
+        '||● Rewrites:||{0.rewrites.count}||']
+    counts = [i.format(au) for i in counts if '||0||' not in i.format(au)]
+
+    ratings = [
+        '||Rating:||{0.pages.rating} ({0.pages.average})||',
+        '||● SCPs:||{0[scp].rating} ({0[scp].average})||',
+        '||● Tales:||{0[tale].rating} ({0[tale].average})||',
+        '||● GOI-format:||{0[goi-format].rating} ({0[goi-format].average})||',
+        '||● Artwork:||{0[artwork].rating} ({0[artwork].average})||',
+        '||● Rewrites:||{0.rewrites.rating} ({0.rewrites.average})||']
+    ratings = [
+        i.format(au) for i in ratings if '||0 (0)||' not in i.format(au)]
+
+    intro = ['[[div class="author-summary"]]']
+    intro.extend(counts)
+    intro.append('|| || ||')
+    intro.extend(ratings)
+    intro.extend(['[[/div]]', '~~~~'])
+
+    articles = [
+        '[[div class="articles"]]',
+        '++ Articles',
+        '||~ Title||~ Rating||~ Tags||~ Link||~ Created||~ Relation||']
+    for p in sorted(au.pages, key=lambda x: x.rating, reverse=True):
+        tags = ', '.join(sorted(p.tags))
+        link = '[[[{}|{}]]]'.format(p.url, p.url.split('/')[-1])
+        relation = p.authors[au.name]
+        template = (
+            '||{0.title}||> {0.rating:+d}||{1}||{2}||{0.created:.10}||= {3}||')
+        articles.append(template.format(p, tags, link, relation))
+    articles.append('[[/div]]')
+
+    return '\n'.join(intro + articles)
+
+###############################################################################
+# Misc
+###############################################################################
+
+
+def get_error_report(pages):
+    output = ''
+    no_tags = ['\x02{}\x02'.format(p.title) for p in pages if not p.tags]
+    if no_tags:
+        output += 'Pages without tags: {}. '.format(', '.join(no_tags))
+    no_title = ['\x02{}\x02'.format(p.title) for p in pages
+                if re.search(r'/scp-[0-9]+$', p.url) and
+                p._raw_title == p.title]
+    if no_title:
+        output += 'Pages without titles: {}.'.format(', '.join(no_title))
+    if output:
+        return output
+    return 'I found no errors.'
