d9fb6d305e1de1ec7f5acd18a132e6c8099bd47c
anqxyr <anqxyr@gmail.com>
2016-04-15 13:45:33 +0300

    update notes

--- jarvis/notes.py
+++ jarvis/notes.py
@@ -16,109 +16,7 @@ from . import db, tools, lexicon
 ###############################################################################
 
 
-def tell(sender, recipient, text):
-    recipient = recipient.strip().lower()
-    sender = str(sender)
-    time = arrow.utcnow().timestamp
-    if not re.match(r'^@?[\w\[\]{}^]+$', recipient):
-        return lexicon.bad_input
-    if not recipient.startswith('@'):
-        db.Tell.create(
-            sender=sender, recipient=recipient,
-            text=text, time=time, topic=None)
-        return lexicon.tell_ok
-    else:
-        users = db.Subscriber.select().where(db.Subscriber.topic == recipient)
-        users = [i.user for i in users]
-        if not users:
-            return lexicon.tell_topic_empty
-        db.Tell.insert_many(dict(
-            sender=sender, recipient=user, text=text,
-            time=time, topic=recipient) for user in users)
-        return lexicon.tell_topic_ok.format(count=len(users))
-
-
-def showtells(user):
-    user = user.strip().lower()
-    query = db.Tell.select().where(db.Tell.recipient == user)
-    for tell in query.execute():
-        time = arrow.get(tell.time).humanize()
-        if not tell.topic:
-            yield '{} said {}: {}'.format(tell.sender, time, tell.message)
-        else:
-            yield '{} said {} via {}: {}'.format(
-                tell.sender, time, tell.topic, tell.message)
-        tell.delete_instance()
-
-
-def notdelivered(user):
-    user = user.strip().lower()
-    query = db.Tell.select().where(
-        fn.Lower(db.Tell.sender) == user, db.Tell.topic == None)
-    if not query.exists():
-        return lexicon.notdelivered_empty
-    users = ', '.join(sorted({i.recipient for i in query}))
-    return lexicon.notdelivered_ok.format(total=query.count(), users=users)
-
-
-def purgetells(user):
-    user = user.strip().lower()
-    query = db.Tell.select().where(
-        fn.Lower(db.Tell.sender) == user, db.Tell.topic == None)
-    if not query.exists():
-        return lexicon.purgetells_empty
-    count = query.count()
-    db.Tell.delete().where(fn.Lower(db.Tell.sender) == user).execute()
-    return lexicon.purgetells_ok.format(count=count)
-
-
-def seen(user, channel):
-    user = user.strip().lower()
-    query = db.Message.select().where(
-        fn.Lower(db.Message.user) == user,
-        db.Message.channel == channel).order_by(db.Message.time.desc())
-    try:
-        msg = query.get()
-    except db.Message.DoesNotExist:
-        return lexicon.seen_never
-    time = arrow.get(msg.time).humanize()
-    return lexicon.seen_ok.format(user=msg.user, time=time, text=msg.text)
-
-
-def quote(inp, channel):
-    inp = inp.strip() if inp else ''
-    parsed = re.match(r'^ *[0-9]* *$', inp)
-    if parsed:
-        return _get_quote(None, channel, parsed.group(0).strip())
-    parsed = re.match(
-        r'^(add|del)? ?(\d{4}-\d{2}-\d{2})? ?([\w\d<>^{}[\]\\-]+)(.*)$', inp)
-    if not parsed:
-        return lexicon.bad_input
-    cmd, time, name, text = parsed.groups()
-    text = text.strip()
-    channel = str(channel).strip()
-    if cmd == 'add':
-        return _add_quote(name, channel, text, time)
-    if cmd == 'del':
-        return _delete_quote(name, channel, text)
-    if name == channel:
-        name = None
-    return _get_quote(name, channel, text)
-
-
-def remember(inp, channel):
-    user, text = inp.split(maxsplit=1)
-    user = user.strip().lower()
-    text = text.strip()
-    db.Rem.delete().where(
-        db.Rem.user == user, db.Rem.channel == channel).execute()
-    db.Rem.create(user=user, channel=channel, text=text)
-    return lexicon.quote_add_ok
-
-###############################################################################
-
-
-def _init():
+def init():
     db.db.connect()
     db.Tell.create_table(True)
     db.Message.create_table(True)
@@ -134,31 +32,121 @@ def _init():
         pass
 
 
-def _log(user, channel, text):
+def log_message(user, channel, text):
     db.Message.create(
         user=user.strip(), channel=channel,
         time=arrow.utcnow().timestamp, text=text)
 
 
-def _add_quote(user, channel, text, time=None):
+def send_tell(sender, recipient, text):
+    recipient = recipient.strip().lower()
+    sender = str(sender)
+    time = arrow.utcnow().timestamp
+    if not re.match(r'^@?[\w\[\]{}^]+$', recipient):
+        return lexicon.bad_input
+    if not recipient.startswith('@'):
+        db.Tell.create(
+            sender=sender, recipient=recipient,
+            text=text, time=time, topic=None)
+        return lexicon.tell.send
+    else:
+        users = db.Subscriber.select().where(db.Subscriber.topic == recipient)
+        users = [i.user for i in users]
+        if not users:
+            return lexicon.topic.no_subscribers
+        db.Tell.insert_many(dict(
+            sender=sender, recipient=user, text=text,
+            time=time, topic=recipient) for user in users)
+        return lexicon.topic.send.format(count=len(users))
+
+
+def get_tells(user):
+    user = user.strip().lower()
+    query = db.Tell.select().where(db.Tell.recipient == user)
+    for tell in query.execute():
+        time = arrow.get(tell.time).humanize()
+        if not tell.topic:
+            yield '{} said {}: {}'.format(tell.sender, time, tell.text)
+        else:
+            yield '{} said {} via {}: {}'.format(
+                tell.sender, time, tell.topic, tell.text)
+        tell.delete_instance()
+
+
+def get_stored_tells_count(user):
+    user = user.strip().lower()
+    query = db.Tell.select().where(
+        fn.Lower(db.Tell.sender) == user, db.Tell.topic.is_null())
+    if not query.exists():
+        return lexicon.tell.storage_empty
+    users = ', '.join(sorted({i.recipient for i in query}))
+    return lexicon.tell.storage_count.format(total=query.count(), users=users)
+
+
+def purge_stored_tells(user):
+    user = user.strip().lower()
+    query = db.Tell.select().where(
+        fn.Lower(db.Tell.sender) == user, db.Tell.topic.is_null())
+    if not query.exists():
+        return lexicon.tell.storage_empty
+    count = query.count()
+    db.Tell.delete().where(fn.Lower(db.Tell.sender) == user).execute()
+    return lexicon.tell.storage_purged.format(count=count)
+
+
+def get_user_seen(user, channel):
+    user = user.strip().lower()
+    query = db.Message.select().where(
+        fn.Lower(db.Message.user) == user,
+        db.Message.channel == channel).order_by(db.Message.time.desc())
+    try:
+        msg = query.get()
+    except db.Message.DoesNotExist:
+        return lexicon.seen.never
+    time = arrow.get(msg.time).humanize()
+    return lexicon.seen.last.format(user=msg.user, time=time, text=msg.text)
+
+
+def dispatch_quote(inp, channel):
+    inp = inp.strip() if inp else ''
+    parsed = re.match(r'^ *[0-9]* *$', inp)
+    if parsed:
+        return get_quote(None, channel, parsed.group(0).strip())
+    parsed = re.match(
+        r'^(add|del)? ?(\d{4}-\d{2}-\d{2})? ?([\w\d<>^{}[\]\\-]+)(.*)$', inp)
+    if not parsed:
+        return lexicon.bad_input
+    cmd, time, name, text = parsed.groups()
+    text = text.strip()
+    channel = str(channel).strip()
+    if cmd == 'add':
+        return add_quote(name, channel, text, time)
+    if cmd == 'del':
+        return delete_quote(name, channel, text)
+    if name == channel:
+        name = None
+    return get_quote(name, channel, text)
+
+
+def add_quote(user, channel, text, time=None):
     user = user.strip().lower()
     if db.Quote.select().where(
             db.Quote.user == user,
             db.Quote.channel == channel, db.Quote.text == text).exists():
-        return lexicon.quote_add_duplicate
+        return lexicon.quote.already_exists
     if not time:
         time = arrow.utcnow().format('YYYY-MM-DD')
     db.Quote.create(user=user, channel=channel, time=time, text=text)
-    return lexicon.quote_add_ok
+    return lexicon.quote.saved
 
 
-def _get_quote(user, channel, index=None):
+def get_quote(user, channel, index=None):
     query = db.Quote.select().where(db.Quote.channel == channel)
     if user:
         user = user.strip().lower()
         query = query.where(db.Quote.user == user)
     if not query.exists():
-        return lexicon.quote_get_not_found
+        return lexicon.quote.none_saved
     if not index:
         index = random.randint(1, query.count())
     else:
@@ -173,19 +161,29 @@ def _get_quote(user, channel, index=None):
         index, query.count(), quote.time, quote.user, quote.text)
 
 
-def _delete_quote(user, channel, text):
+def delete_quote(user, channel, text):
     user = user.strip().lower()
     query = db.Quote.select().where(
         db.Quote.user == user,
         db.Quote.channel == channel,
         db.Quote.text == text)
     if not query.exists():
-        return lexicon.quote_del_not_found
+        return lexicon.quote.not_found
     query.get().delete_instance()
-    return lexicon.quote_del_ok
+    return lexicon.quote.deleted
 
 
-def _get_rem(user, channel):
+def remember_user(inp, channel):
+    user, text = inp.split(maxsplit=1)
+    user = user.strip().lower()
+    text = text.strip()
+    db.Rem.delete().where(
+        db.Rem.user == user, db.Rem.channel == channel).execute()
+    db.Rem.create(user=user, channel=channel, text=text)
+    return lexicon.quote.saved
+
+
+def recall_user(user, channel):
     user = user.strip().lower()
     try:
         rem = db.Rem.select().where(
