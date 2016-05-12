8e6db8e444099a9669775707d85f47de11a35b6d
anqxyr <anqxyr@gmail.com>
2016-05-12 18:27:44 +0300

    and tests again

--- jarvis/tests/test_notes.py
+++ jarvis/tests/test_notes.py
@@ -48,6 +48,20 @@ def run(fn, inp, *args, **kwargs):
     return results[0] if len(results) == 1 else results
 
 ###############################################################################
+
+
+def st_user():
+    user = st.characters(blacklist_categories=['Cc', 'Cs', 'Zs', 'Zl', 'Zp'])
+    user = user.filter(lambda x: x.strip().rstrip(':,'))
+    user = user.filter(lambda x: not x.strip().startswith('@'))
+    user = user.filter(lambda x: not x.isdigit())
+    return user
+
+
+def st_msg():
+    return st.text().filter(str.strip)
+
+###############################################################################
 # Tell
 ###############################################################################
 
@@ -57,18 +71,15 @@ def test_tell_random_input(inp):
     run(notes.tell, inp)
 
 
-@hy.given(st.text())
-def test_tell_send_user_successful(inp):
-    """Test notes.tell command."""
-    hy.assume(not inp.strip().startswith('@'))
-    hy.assume(len(inp.split()) > 1)
-    hy.assume(inp.split()[0].strip(':,'))
+@hy.given(st_user(), st_msg())
+def test_tell_send_user_successful(user, msg):
     count = notes.Tell.select().count()
+    inp = user + ' ' + msg
     assert run(notes.tell, inp) == lexicon.tell.send
     assert notes.Tell.select().count() == count + 1
 
 
-@hy.given(st.none())
+@hy.given(st.none() | st_user())
 def test_tell_send_failed(inp):
     """Test notes.tell command."""
     assert run(notes.tell, inp) == notes.tell._usage
@@ -149,12 +160,12 @@ def test_seen_never():
     assert run(notes.seen, '   ') == lexicon.seen.never
 
 
-@hy.given(st.characters(blacklist_categories='ZC'))
+@hy.given(st_user())
 @hy.example('anqxyr')
 @hy.example('test-user')
-def test_seen_case_insensitive(inp):
-    upper = run(notes.seen, inp.upper())
-    lower = run(notes.seen, inp.lower())
+def test_seen_case_insensitive(user):
+    upper = run(notes.seen, user.upper())
+    lower = run(notes.seen, user.lower())
     assert upper == lower
     assert upper.user == lower.user
     assert upper.text == lower.text
@@ -171,28 +182,77 @@ def test_quote_random_input(inp):
 
 
 def test_quote_usage_messages():
-    assert run(notes.quote, '') == notes.quote._usage
+    assert run(notes.quote, '--help') == notes.quote._usage
     assert run(notes.quote, 'add') == notes.quote_add._usage
     assert run(notes.quote, 'del') == notes.quote_del._usage
 
 
-@hy.given(st.text())
-def test_quote_add_successful(inp):
-    hy.assume(len(inp.split()) > 1)
-    run(notes.quote, 'del ' + inp)
-    assert run(notes.quote, 'add ' + inp) == lexicon.quote.saved
+@hy.given(st_user(), st_msg())
+def test_quote_add_successful(user, msg):
+    inp = ' {} {}'.format(user, msg)
+    run(notes.quote, 'del' + inp)
+    assert run(notes.quote, 'add' + inp) == lexicon.quote.saved
 
 
-@hy.given(st.text())
-def test_quote_add_already_exists(inp):
-    hy.assume(len(inp.split()) > 1)
-    run(notes.quote, 'add ' + inp)
-    assert run(notes.quote, 'add ' + inp) == lexicon.quote.already_exists
+@hy.given(st_user(), st_msg())
+def test_quote_add_already_exists(user, msg):
+    inp = ' {} {}'.format(user, msg)
+    run(notes.quote, 'add' + inp)
+    assert run(notes.quote, 'add' + inp) == lexicon.quote.already_exists
 
 
-@hy.given(hydt.dates(), st.text())
-def test_quote_add_date(date, inp):
-    hy.assume(len(inp.split()) > 1)
-    run(notes.quote, 'del ' + inp)
-    result = run(notes.quote, 'add {} {}'.format(date.isoformat(), inp))
-    assert result == lexicon.quote.saved
+@hy.given(hydt.dates(), st_user(), st_msg())
+def test_quote_add_date(date, user, msg):
+    run(notes.quote, 'del {} {}'.format(user, msg))
+    assert run(notes.quote, 'add {} {} {}'.format(
+        date.isoformat(), user, msg)) == lexicon.quote.saved
+
+
+@hy.settings(suppress_health_check=[hy.HealthCheck.random_module])
+def test_quote_get_random():
+    quote = run(notes.quote, '')
+    assert quote == lexicon.quote.get
+    assert quote.text
+
+
+@hy.given(st.integers().filter(lambda x: x > 0))
+def test_quote_get_index(index):
+    query = notes.Quote.select().where(notes.Quote.channel == 'test-channel')
+    hy.assume(index <= query.count())
+    quote = run(notes.quote, str(index))
+    assert quote == lexicon.quote.get
+    assert quote.index == index
+
+
+@hy.given(st.integers().filter(lambda x: x <= 0))
+def test_quote_get_index_negative_fail(index):
+    assert run(notes.quote, str(index)) == notes.quote_get._usage
+
+
+@hy.given(st.integers().filter(lambda x: x > 0))
+def test_quote_get_index_too_big(index):
+    query = notes.Quote.select().where(notes.Quote.channel == 'test-channel')
+    hy.assume(index > query.count())
+    assert run(notes.quote, str(index)) == lexicon.input.bad_index
+
+
+@hy.settings(suppress_health_check=[hy.HealthCheck.random_module])
+@hy.given(st_user())
+def test_quote_get_user(user):
+    run(notes.quote, 'add {} test-quote'.format(user))
+    quote = run(notes.quote, user)
+    assert quote.user.lower() == user.lower()
+
+
+@hy.given(st_user(), st.integers().filter(lambda x: x > 0))
+def test_quote_get_user_and_index(user, index):
+    query = (
+        notes.Quote.select()
+        .where(notes.Quote.channel == 'test-channel')
+        .where(notes.peewee.fn.Lower(notes.Quote.user) == user.lower()))
+    hy.assume(index <= query.count())
+    quote = run(notes.quote, user + ' ' + str(index))
+    assert quote == lexicon.quote.get
+    assert quote.user.lower() == user.lower()
+
+###############################################################################
