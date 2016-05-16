6db72c7c5eb8ccafce81f896c64bd72170d2d9bb
anqxyr <anqxyr@gmail.com>
2016-05-16 14:00:25 +0300

    improve author page generation

--- jarvis/stats.py
+++ jarvis/stats.py
@@ -4,12 +4,149 @@
 # Module Imports
 ###############################################################################
 
-import pathlib
-import pygal
 import pyscp
+import textwrap
+
+from . import core
 
 
-from . import core, gviz
+###############################################################################
+# Templates
+###############################################################################
+
+
+CHART = """
+google.charts.setOnLoadCallback({name});
+
+function {name}() {{
+    var data = new google.visualization.arrayToDataTable([
+{data}
+        ]);
+
+    var options = {options};
+
+    var chart = new google.visualization.{class_name}(
+        document.getElementById('{name}'));
+
+    chart.draw(data, options);
+}}
+"""
+
+USER = """
+[[html]]
+<base target="_parent" />
+
+<style type="text/css">
+@import url(http://scp-stats.wdfiles.com/local--theme/scp-stats/style.css);
+</style>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js">
+</script>
+
+<script type="text/javascript">
+google.charts.load('current', {{'packages':['table', 'corechart']}});
+{articles_chart}
+{articles_table}
+</script>
+
+<div id="articles_chart"></div>
+<div style="clear: both;"></div>
+<h2>Articles</h2>
+<div id="articles_table"></div>
+
+[[/html]]
+"""
+
+###############################################################################
+# Helper Functions
+###############################################################################
+
+
+def html(tag, text, **kwargs):
+    if 'cls' in kwargs:
+        kwargs['class'] = kwargs.pop('cls')
+    attrs = ' '.join('{}="{}"'.format(k, v) for k, v in kwargs.items())
+    if attrs:
+        attrs = ' ' + attrs
+    return '<{tag}{attrs}>{text}</{tag}>'.format(
+        tag=tag, text=text, attrs=attrs)
+
+###############################################################################
+# Chart Classes
+###############################################################################
+
+
+class Chart:
+
+    def __init__(self, pages):
+        for p in pages:
+            self.add_page(p)
+
+    def format_row(self, row, indent):
+        row = ',\n'.join(map(repr, row))
+        row = textwrap.indent(row, '    ')
+        row = '[\n{}\n]'.format(row)
+        return textwrap.indent(row, ' ' * indent)
+
+    def render(self):
+        data = ',\n'.join([self.format_row(r, 8) for r in self.data])
+        return CHART.format(
+            name=self.name,
+            class_name=self.class_name,
+            data=data,
+            options=self.options)
+
+
+class ArticlesChart(Chart):
+
+    def __init__(self, pages):
+        self.name = 'articles_chart'
+        self.class_name = 'ColumnChart'
+        self.data = [
+            ['Title', 'Rating', {'role': 'tooltip'}, {'role': 'style'}]]
+        self.options = {}
+        super().__init__(pages)
+
+    def add_page(self, page):
+        if 'scp' in page.tags:
+            color = 'color: #db4437'
+        elif 'tale' in page.tags:
+            color = 'color: #4285f4'
+        else:
+            color = 'color: #f4b400'
+
+        self.data.append([
+            page.title,
+            page.rating,
+            page.created[:10],
+            color])
+
+
+class ArticlesTable(Chart):
+
+    def __init__(self, pages, user):
+        self.name = 'articles_table'
+        self.class_name = 'Table'
+        self.user = user
+        self.data = ['Title Rating Tags Link Created Relation'.split()]
+        self.options = {
+            'showRowNumber': 'True',
+            'allowHtml': 'True',
+            'sortColumn': 1,
+            'width': '100%'}
+        super().__init__(pages)
+
+    def add_page(self, page):
+        tags = [html('b', t) if t in 'scp tale hub admin author' else t
+                for t in page.tags]
+        tags = ', '.join(sorted(tags))
+
+        link = html('a', page.url.split('/')[-1], href=page.url)
+
+        rel = page.metadata[self.user][0]
+        rel = html('span', rel, cls='rel-' + rel)
+
+        self.data.append([
+            page.title, page.rating, tags, link, page.created[:10], rel])
 
 
 ###############################################################################
@@ -19,100 +156,12 @@ def update_user(name):
     wiki = pyscp.wikidot.Wiki('scp-stats')
     wiki.auth(core.config['wiki']['name'], core.config['wiki']['pass'])
     p = wiki('user:' + name.lower())
-    p.create(get_user(name), title=name, comment='automated update')
-    return p.url
 
-###############################################################################
-
-
-def user_summary(name):
-    row = '||{0}||{1.count}||{1.rating}||{1.average}||'.format
-    primary = core.pages.primary(name)
-    tags = [row(*i) for i in primary.split_page_type().items()]
-    rels = [row(*i) for i in core.pages.articles.split_relation(name).items()]
-    return '\n'.join([
-        '[[div class="stats"]]',
-        '||~ Category||~ Page Count||~ Net Rating||~ Average||',
-        row('Total', primary),
-        '||||||||~ ||', '\n'.join(tags),
-        '||||||||~ ||', '\n'.join(rels),
-        '[[/div]]'])
-
-
-def user_articles(name):
-    table = gviz.Table('articles')
-
-    table.add_column('Title')
-    table.add_column('Rating', 'number')
-    table.add_column('Tags')
-    table.add_column('Link')
-    table.add_column('Created')
-    table.add_column('Relation')
-
-    pages = [p for p in core.pages.related(name) if p.tags]
-    for p in sorted(pages, key=lambda x: x.rating, reverse=True):
-        tags = ['<b>{}</b>'.format(t) if t in 'scp tale hub admin author'
-                else t for t in p.tags]
-        tags = ', '.join(sorted(tags))
-        link = '<a href={}>{}</a>'.format(p.url, p.url.split('/')[-1])
-        relation = '<span class="rel-{0}">{0}</span>'.format(
-            p.metadata[name][0])
-        table.add_row(p.title, p.rating, tags, link, p.created[:10], relation)
-    return table.render()
-
-
-def plot_user_pages(name):
     pages = core.pages.related(name).articles
-    plot = pygal.Bar(
-        style=pygal.style.CleanStyle,
-        show_x_labels=False,
-        show_legend=False, show_y_guides=False,
-        margin=0, width=800, height=400)
-
     pages = sorted(pages, key=lambda x: x.created)
-    plot.x_labels = [p.created[:10] for p in pages]
-    data = []
-    for p in pages:
-        if 'scp' in p.tags:
-            color = '#DC0000'
-        elif 'tale' in p.tags:
-            color = '#4040DD'
-        else:
-            color = '#FF9900'
-        data.append({'value': p.rating, 'label': p.title, 'color': color})
-    plot.add('Rating', data)
+    data = USER.format(
+        articles_chart=ArticlesChart(pages).render(),
+        articles_table=ArticlesTable(pages, name).render())
 
-    path = 'images/users/{}/'.format(name)
-    try:
-        pathlib.Path(path).mkdir(parents=True)
-    except FileExistsError:
-        pass
-    plot.render_to_file(path + 'pages.svg')
-    return '{}:8000/users/{}/pages.svg'.format(
-        core.config['aws']['address'], name)
-
-
-def get_plot(url, css_class):
-    data = [
-        '[[div id="iframe-plot-pages"]]',
-        '[[html]]',
-        '<div class="plot-pages">',
-        '<embed type="image/svg+xml" src="{}"/>',
-        '</div>',
-        '[[/html]]',
-        '[[/div]]']
-    return '\n'.join(data).format(url)
-
-
-def get_user(name):
-    source = [
-        get_plot(plot_user_pages(name), 'plot-pages'),
-        user_summary(name),
-        '~~~~',
-        '++ Articles',
-        '[[div id="iframe-articles"]]',
-        '[[html]]',
-        user_articles(name),
-        '[[/html]]',
-        '[[/div]]']
-    return '\n'.join(source)
+    p.create(data, title=name, comment='automated update')
+    return p.url
