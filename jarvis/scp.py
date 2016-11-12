79df1cee34d375f216e649e97b09772d716d3806
anqxyr <anqxyr@gmail.com>
2016-11-12 13:10:02 +0300

    fix bug in orphaned titles

--- jarvis/scp.py
+++ jarvis/scp.py
@@ -208,7 +208,7 @@ def errors_orphaned():
     urls.extend([p.url for p in core.wiki.list_pages(
         name='scp-*', created_at='last 3 hours')])
     pages = [k for k in core.wiki.titles() if k not in urls]
-    pages = [p for p in pages if re.search(r'/scp-[0-9]+$', p)]
+    pages = [p for p in pages if re.search(r'/scp-[0-9]{3,4}$', p)]
     return map(core.wiki, pages)
 
 
